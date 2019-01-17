493f2daf2b9f8861879491c4c1c181df19a38620
Muhammad Quwais Safutra <45951646+BRAKOMANDA@users.noreply.github.com>
2019-01-18 00:50:58 +0700

    Create for_loop.py

---
+++ Console/V.1.0/for_loop.py
@@ -0,0 +1,13 @@
+# Author : Jenna Lolita
+
+# tanpa new line
+x range(in 1,100)
+  print x
+  
+# dengan new line
+x range(in 1,100)
+  print x.inline
+  
+# terimakasih devansyah!
+# atas print statements nya
+# heheh
