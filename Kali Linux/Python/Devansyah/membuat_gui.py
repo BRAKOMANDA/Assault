157e2565237c316c1c2ae748b518079ba9beb367
Muhammad Quwais Safutra <45951646+BRAKOMANDA@users.noreply.github.com>
2019-01-18 01:06:56 +0700

    Create membuat_gui.py

---
+++ Kali Linux/Python/Devansyah/membuat_gui.py
@@ -0,0 +1,42 @@
+# requirement
+# python 2.7
+# tkinter
+
+from devansyah import riupassa
+
+# set variable 
+dev = devansyah.riupassa()
+
+## membuat form ##
+def buatForm(self):
+  textLabel = dev.labelGuiOfText('judul label')
+  iconLabel = dev.lavelGuiOfIcon('icon gui')
+  def __init__(dev.form):
+    # disini saya ingin
+    # mengatur besar gui
+    # form nya
+    dev.form(400,500)
+  def __init__(dev.OnOptions):
+    # mengatur pengaturan choice.
+    # bisa mendisable button
+    # choiches loh wueheheh 
+    a = dev.StripLiteButton(True) # true? aktifkan
+    b = dev.OnFormOpenFull(False) # false? disable
+    c = dev.CloseReizxan(True)    # mengaktifkan close
+    def __all__onclick__math__init():
+      # ketika klik
+      dev.__onclick__(a):
+        dev.LetsGoDown
+      dev.__onclick__(b):
+        pass
+      dev.__onclick__(c):
+        dev.CloseNow
+  def __init__(dev.isiform):
+    # tampilkan kata hello world berwarna
+    # hitam di tengah label size 40px
+    dev.AddText("hello world!",
+                 display_on_center,
+                 size=40px,
+                 color="HITAM")
+                 
+yes_show_now(dev).buatForm()
