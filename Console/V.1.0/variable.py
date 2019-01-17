34692af7cea81ec4d88e4338441c732e45557d7a
Muhammad Quwais Safutra <45951646+BRAKOMANDA@users.noreply.github.com>
2019-01-18 00:54:31 +0700

    Create variable.py

---
+++ Console/V.1.0/variable.py
@@ -0,0 +1,14 @@
+# By : Misin Abdullah
+
+a = 10       # int
+b = 20.33    # double
+c = "hello"  # string atau char
+
+# ingin mencetak mereka?
+print a
+print c
+
+# ingin sum mereka?
+e = 30
+d = a+e
+print d
