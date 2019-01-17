eef00fc2dc5eb567acb4f554b4001c857e7d5640
Muhammad Quwais Safutra <45951646+BRAKOMANDA@users.noreply.github.com>
2019-01-18 00:44:49 +0700

    Create Hello_world.py

---
+++ Console/V.1.0/Hello_world.py
@@ -0,0 +1,12 @@
+# By : Devansyah Riupassa
+
+# File Ini Dibuat Dengan Python menggunakan Lexer
+
+# print hello world
+print "hello world!"
+
+# print with symbol kutip (")
+p "hello world"
+
+# print with symbol kutip satu (')
+print("hello world!")
